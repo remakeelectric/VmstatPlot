8541b716da449f1f432bb8ff938bb2d6d53a0db3
Karl Palsson <karlp@remake.is>
2013-10-08 14:31:40 +0000

    Make graphs on a less custom set of filenames

--- graph.sh
+++ graph.sh
@@ -5,11 +5,8 @@
 MACHINE_MEM=512
 
 cd $1
-for lf in *app*.log; do
-  ../vmplot.py -t $lf -c 21 -k 1024 -m 4096 $lf
-done
-for lf in *lb*.log *db*log; do
-  ../vmplot.py -t $lf -c 21 -k 1024 -m 2048 $lf
+for lf in collected-*-output.log; do
+  ../vmplot.py -t $lf -c 20 -k 1024 -m $MACHINE_MEM $lf
 done
 ls -1 *.png |sort |sed -e 's/^/<img src="/' -e 's/$/"><br>/' >index.html
 cd ..
